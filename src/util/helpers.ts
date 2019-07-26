2f4e46967c5924802aa1224240d693608108c83b
Mike Sandula <msandula@gmail.com>
2019-07-26 15:54:38 -0400

    fix: getNumberOfNeighbors function

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -9,31 +9,47 @@ export const generateGridRows = (rows: number, columns: number): IGrid => {
         cellIndex: j,
         isFlagged: false,
         isMine: Math.random() > 0.75, // TODO: Assign dynamically
-        isOpen: false,
-        neighborsWithMines: Math.floor(Math.random() * 5), // TODO: Assess dynamically
+        isOpen: true,
+        neighborsWithMines: 0,
         rowIndex: i
       };
       row.push(basicSquare);
     }
     grid.push(row);
   }
+
+  // Once mines are in place, figure out the numberOfNeighbors each square has
+  grid.forEach(row => {
+    row.forEach(square => {
+      square.neighborsWithMines = getNumberOfNeighbors(square, grid);
+    });
+  });
+
   return grid;
 };
 
 export const getNumberOfNeighbors = (square: ISquare, grid: IGrid): number => {
-  const row = grid[square.rowIndex];
-  const rowAbove = grid[square.rowIndex - 1];
-  const rowBelow = grid[square.rowIndex + 1];
+  const rowOfSquare = grid[square.rowIndex];
+
+  const isFirstInRow = square.cellIndex === 0;
+  const isLastInRow = square.cellIndex === rowOfSquare.length - 1;
+
+  const rowAbove = square.rowIndex > 0 ? grid[square.rowIndex - 1] : [];
+  const rowBelow =
+    square.rowIndex < grid.length - 1 ? grid[square.rowIndex + 1] : [];
 
   const neighbors = [
-    ...rowAbove.slice(square.cellIndex - 1, square.cellIndex + 1),
-    row[square.cellIndex - 1],
-    row[square.cellIndex + 1],
-    ...rowBelow.slice(square.cellIndex - 1, square.cellIndex + 1)
-  ];
+    ...rowAbove.slice(
+      isFirstInRow ? 0 : square.cellIndex - 1,
+      isLastInRow ? rowAbove.length : square.cellIndex + 2
+    ), // The squares directly above it
+    rowOfSquare[square.cellIndex - 1], // The square to its left
+    rowOfSquare[square.cellIndex + 1], // The square to its right
+    ...rowBelow.slice(
+      isFirstInRow ? 0 : square.cellIndex - 1,
+      isLastInRow ? rowBelow.length : square.cellIndex + 2
+    ) // The squares directly below it
+  ].filter(Boolean);
 
-  console.log('neighbors: ', neighbors);
-
-  const neighborsWithMines = neighbors.filter(neighbor => neighbor.isMine);
-  return neighborsWithMines.length;
+  return neighbors.filter(neighbor => neighbor.isMine).length;
 };

5593d5740144171be38b6db0c38db1465ddf4b4f
Mike Sandula <msandula@gmail.com>
2019-08-04 08:25:50 -0400

    style and revise rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -5,25 +5,27 @@ import Icon from './Icon';
 const Rules = () => {
   return (
     <>
-      <ul>
-        <li>Left-click an empty square to reveal it</li>
-        <li>
-          Right-click (or <code>ctrl+click</code> on Windows,{' '}
-          <code>cmd+click</code> on Mac) an empty square to flag it (
-          <Icon name="flag-checkered" />)
-        </li>
-        <li>
-          Midde-click (or left+right click) a number to reveal its adjacent
-          squares
-        </li>
-        <li>
-          Press <code>space</code> while hovering over a square to flag it or
-          reveal its adjacent squares
-        </li>
-        <li>
-          Press <code>F2</code> to start a new game
-        </li>
-      </ul>
+      <h3 className="mono">New game</h3>
+      <p className="indented-s">
+        Press <code>F2</code> to start a new game
+      </p>
+
+      <h3 className="mono">Open a square</h3>
+      <p className="indented-s">Left-click an empty square to reveal it</p>
+
+      <h3 className="mono">
+        Flag a square (
+        <Icon name="flag-checkered" />)
+      </h3>
+      <p className="indented-s">
+        If you think a square has a mine, you can flag it by right-clicking it
+        or hovering over it and pressing <code>space</code>
+      </p>
+
+      <h3 className="mono">View adjacent squares</h3>
+      <p className="indented-s">
+        Midde-click (or left+right click) a square to show its adjacent squares
+      </p>
     </>
   );
 };
